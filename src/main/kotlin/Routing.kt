24726626b93e7edb954c928ff6174f2a06698422
AnTei404 <antonio.lourinho.teixeira@gmail.com>
2025-05-16 01:58:53 +0100

    New Betting system

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -48,16 +48,53 @@ fun Application.configureRouting() {
             }
         }
 
-        post("/casino/blackjack/hit") {
+        post("/casino/blackjack/bet") {
             val player = call.sessions.get<Player>()
-            if (player != null) {
-                val gameState = blackjack.hit()
+            val chipsBet = call.receiveParameters()["chipsBet"]?.toIntOrNull()
+            if (player != null && chipsBet != null && chipsBet > 0 && chipsBet <= player.chips) {
+                val updatedPlayer = player.copy(lastBet = chipsBet, chips = player.chips - chipsBet)
+                call.sessions.set(updatedPlayer)
+                val gameState = blackjack.startGame()
                 call.respond(
                     ThymeleafContent(
                         "blackjack",
                         mapOf(
                             "name" to player.name,
-                            "gameState" to gameState
+                            "gameState" to gameState,
+                            "chipsBet" to chipsBet,
+                            "chips" to updatedPlayer.chips
+                        )
+                    )
+                )
+            } else {
+                call.respondText("Invalid bet", status = HttpStatusCode.BadRequest)
+            }
+        }
+
+        post("/casino/blackjack/hit") {
+            val player = call.sessions.get<Player>()
+            if (player != null) {
+                val gameState = blackjack.hit()
+                val chipsBet = player.lastBet ?: 0
+                var resultMessage = ""
+                var updatedChips = player.chips
+                if (gameState.gameOver) {
+                    when (gameState.gameState) {
+                        "Bust! You lose." -> {
+                            resultMessage = "You lost $chipsBet chips and now have $updatedChips chips."
+                        }
+                        else -> {}
+                    }
+                }
+                call.respond(
+                    ThymeleafContent(
+                        "blackjack",
+                        mapOf(
+                            "name" to player.name,
+                            "gameState" to gameState,
+                            "chipsBet" to chipsBet,
+                            "chips" to updatedChips,
+                            "resultMessage" to resultMessage
                         )
                     )
                 )
@@ -70,12 +107,33 @@ fun Application.configureRouting() {
             val player = call.sessions.get<Player>()
             if (player != null) {
                 val gameState = blackjack.stand()
+                val chipsBet = player.lastBet ?: 0
+                var resultMessage = ""
+                var updatedPlayer = player
+                when (gameState.gameState) {
+                    "You win!" -> {
+                        updatedPlayer = player.copy(chips = player.chips + chipsBet * 2)
+                        resultMessage = "You won $chipsBet chips and now have ${updatedPlayer.chips} chips."
+                    }
+                    "You lose!" -> {
+                        // No chips refunded
+                        resultMessage = "You lost $chipsBet chips and now have ${player.chips} chips."
+                    }
+                    "It's a tie!" -> {
+                        updatedPlayer = player.copy(chips = player.chips + chipsBet)
+                        resultMessage = "It's a tie! Your bet is returned. You have ${updatedPlayer.chips} chips."
+                    }
+                }
+                call.sessions.set(updatedPlayer)
                 call.respond(
                     ThymeleafContent(
                         "blackjack",
                         mapOf(
-                            "name" to player.name,
-                            "gameState" to gameState
+                            "name" to updatedPlayer.name,
+                            "gameState" to gameState,
+                            "chipsBet" to chipsBet,
+                            "chips" to updatedPlayer.chips,
+                            "resultMessage" to resultMessage
                         )
                     )
                 )
@@ -86,17 +144,38 @@ fun Application.configureRouting() {
 
         post("/casino/blackjack/restart") {
             val player = call.sessions.get<Player>()
-            if (player != null) {
-                val gameState = blackjack.restartGame()
-                call.respond(
-                    ThymeleafContent(
-                        "blackjack",
-                        mapOf(
-                            "name" to player.name,
-                            "gameState" to gameState
+            val lastBet = player?.lastBet
+            if (player != null && lastBet != null) {
+                if (player.chips >= lastBet) {
+                    val updatedPlayer = player.copy(chips = player.chips - lastBet)
+                    call.sessions.set(updatedPlayer)
+                    val gameState = blackjack.restartGame()
+                    call.respond(
+                        ThymeleafContent(
+                            "blackjack",
+                            mapOf(
+                                "name" to player.name,
+                                "gameState" to gameState,
+                                "chipsBet" to lastBet,
+                                "chips" to updatedPlayer.chips
+                            )
                         )
                     )
-                )
+                } else {
+                    val gameState = blackjack.restartGame()
+                    call.respond(
+                        ThymeleafContent(
+                            "blackjack",
+                            mapOf(
+                                "name" to player.name,
+                                "gameState" to gameState,
+                                "chipsBet" to lastBet,
+                                "chips" to player.chips,
+                                "error" to "You don't have enough."
+                            )
+                        )
+                    )
+                }
             } else {
                 call.respondRedirect("/index.html")
             }
